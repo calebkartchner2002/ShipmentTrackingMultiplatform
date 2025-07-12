71f50c933a411b266bcc0258b0f060611ef0919d
Caleb Kartchner <calebkartchner2002@gmail.com>
2025-07-12 01:40:07 -0400

    final review of UML diagram

--- composeApp/src/desktopMain/kotlin/ShipmentTrackerScreen.kt
+++ composeApp/src/desktopMain/kotlin/ShipmentTrackerScreen.kt
@@ -4,11 +4,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun ShipmentTrackerScreen(viewHelper: TrackerViewHelper) {
